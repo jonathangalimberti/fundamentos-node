0be68a258f94c13b4f50ae5a9f6fe22be303e507
jona-11 <jonathangalimberti@hotmail.com>
2022-10-03 19:55:12 -0300

    ejemplos de variables globales

---
+++ modulos/globales.js
@@ -0,0 +1,22 @@
+//ejemplos de variables globales
+
+console.log()
+
+console.error
+
+setInterval(()=>{
+
+},1000)
+
+setTimeout(() => {
+    
+}, 1000);
+
+//crear variable global
+
+global.miVariable = 'el valor'
+
+console.log(miVariable)
+
+
+
